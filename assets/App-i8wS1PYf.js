7edd43a68442880ba676a805c30bdcecf7be1d25
yyc <2623044395@qq.com>
2024-09-25 13:43:29 +0800

    Updates

--- assets/App-i8wS1PYf.js
+++
@@ -1,1 +0,0 @@
-import{j as s,O as t}from"./index--6yD6AJi.js";function e(){return s.jsx("div",{className:"app",children:s.jsx(t,{})})}export{e as default};
---
+++ assets/App-sKOsMj_V.js
@@ -0,0 +1,1 @@
+import{j as s,O as t}from"./index-2ze4iXUV.js";function e(){return s.jsx("div",{className:"app",children:s.jsx(t,{})})}export{e as default};
