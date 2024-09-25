ae64b24226ee3fabba4fb8b75c1119809d332f14
yyc <2623044395@qq.com>
2024-09-25 14:07:21 +0800

    feat: 添加base基础资源加载路径

--- src/pages/demo/index.tsx
+++ src/pages/demo/index.tsx
@@ -21,7 +21,7 @@ const Index = () => {
   return (
     <>
       <div className={styles.container}>
-        <div className={styles.top}>demo</div>
+        <div className={styles.top}>demo，欢迎来到我的世界！</div>
         <div className={styles.icon}>
           <CameraSvg />
         </div>
